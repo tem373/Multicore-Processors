b175360a713fc094d184da5e4b841b6ed33103a2
tem373 <tem373@nyu.edu>
2019-04-18 14:16:47 -0400

    Sequential algorithm working

--- Lab2/eqns/solve.c
+++ Lab2/eqns/solve.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 
-/*** Skeleton for Lab 1 ***/
+/*** Skeleton for Lab 2 ***/
 
 /***** Globals ******/
 float **a; /* The coefficients */
@@ -28,38 +28,33 @@ void get_input();  /* Read input from file */
    1. diagonal element >= sum of all other elements of the row
    2. At least one diagonal element > sum of all other elements of the row
  */
-void check_matrix()
-{
-  int bigger = 0; /* Set to 1 if at least one diag element > sum  */
-  int i, j;
-  float sum = 0;
-  float aii = 0;
-  
-  for(i = 0; i < num; i++)
-  {
-    sum = 0;
-    aii = fabs(a[i][i]);
-    
-    for(j = 0; j < num; j++)
-       if( j != i)
-	 sum += fabs(a[i][j]);
-       
-    if( aii < sum)
-    {
-      printf("The matrix will not converge.\n");
-      exit(1);
+void check_matrix() {
+    int bigger = 0; /* Set to 1 if at least one diag element > sum  */
+    int i, j;
+    float sum = 0;
+    float aii = 0;
+
+    for(i = 0; i < num; i++) {
+        sum = 0;
+        aii = fabs(a[i][i]);
+
+        for(j = 0; j < num; j++) {
+            if (j != i)
+                sum += fabs(a[i][j]);
+            if (aii < sum) {
+                printf("The matrix will not converge.\n");
+                exit(1);
+            }
+
+            if (aii > sum) {
+                bigger++;
+            }
+        }
     }
-    
-    if(aii > sum)
-      bigger++;
-    
-  }
-  
-  if( !bigger )
-  {
-     printf("The matrix will not converge\n");
-     exit(1);
-  }
+    if(!bigger) {
+        printf("The matrix will not converge\n");
+        exit(1);
+    }
 }
 
 
@@ -72,119 +67,147 @@ void check_matrix()
  * num will have number of variables
  * err will have the absolute error that you need to reach
  */
-void get_input(char filename[])
-{
-  FILE * fp;
-  int i,j;  
+void get_input(char filename[]) {
+    FILE * fp;
+    int i,j;
+
+    fp = fopen(filename, "r");
+    if(!fp) {
+        printf("Cannot open file %s\n", filename);
+        exit(1);
+    }
+
+    fscanf(fp,"%d ",&num);
+    fscanf(fp,"%f ",&err);
+
+    /* Now, time to allocate the matrices and vectors */
+    a = (float**)malloc(num * sizeof(float*));
+    if(!a) {
+        printf("Cannot allocate a!\n");
+        exit(1);
+    }
+
+    for(i = 0; i < num; i++) {
+        a[i] = (float *)malloc(num * sizeof(float));
+        if(!a[i]) {
+            printf("Cannot allocate a[%d]!\n",i);
+            exit(1);
+        }
+    }
+
+    x = (float *) malloc(num * sizeof(float));
+    if(!x) {
+        printf("Cannot allocate x!\n");
+        exit(1);
+    }
+
+    b = (float *) malloc(num * sizeof(float));
+    if(!b) {
+        printf("Cannot allocate b!\n");
+        exit(1);
+    }
+
+    /* Now .. Filling the blanks */
+    /* The initial values of Xs */
+    for(i = 0; i < num; i++) {
+        fscanf(fp, "%f ", &x[i]);
+    }
+    for(i = 0; i < num; i++) {
+        for(j = 0; j < num; j++) {
+            fscanf(fp, "%f ", &a[i][j]);
+        }
+        /* reading the b element */
+        fscanf(fp,"%f ",&b[i]);
+    }
  
-  fp = fopen(filename, "r");
-  if(!fp)
-  {
-    printf("Cannot open file %s\n", filename);
-    exit(1);
-  }
-
- fscanf(fp,"%d ",&num);
- fscanf(fp,"%f ",&err);
-
- /* Now, time to allocate the matrices and vectors */
- a = (float**)malloc(num * sizeof(float*));
- if( !a)
-  {
-	printf("Cannot allocate a!\n");
-	exit(1);
-  }
-
- for(i = 0; i < num; i++) 
-  {
-    a[i] = (float *)malloc(num * sizeof(float)); 
-    if( !a[i])
-  	{
-		printf("Cannot allocate a[%d]!\n",i);
-		exit(1);
-  	}
-  }
- 
- x = (float *) malloc(num * sizeof(float));
- if( !x)
-  {
-	printf("Cannot allocate x!\n");
-	exit(1);
-  }
-
-
- b = (float *) malloc(num * sizeof(float));
- if( !b)
-  {
-	printf("Cannot allocate b!\n");
-	exit(1);
-  }
-
- /* Now .. Filling the blanks */ 
-
- /* The initial values of Xs */
- for(i = 0; i < num; i++)
-	fscanf(fp,"%f ", &x[i]);
- 
- for(i = 0; i < num; i++)
- {
-   for(j = 0; j < num; j++)
-     fscanf(fp,"%f ",&a[i][j]);
-   
-   /* reading the b element */
-   fscanf(fp,"%f ",&b[i]);
- }
- 
- fclose(fp); 
+    fclose(fp);
 
 }
 
+int updateUnknowns() {
+    float x_newarr[num];
+    int done = 1;
+    // Update value element wise
+    for (int i=0; i < num; i++) {
+        float x_old = x[i];
+        float x_new = b[i]; // initialize to coefficient
+        for (int j=0; j < num; j++) {
+            // Use the equation to update the values of
+            if (i != j) {
+                x_new -= a[i][j] * x[j];
+            }
+        }
+        x_new /= a[i][i];
+
+        // Check error
+        float rel_err = fabs((x_new - x_old) / x_new);
+        if (rel_err >= err) {
+            done = 0;
+        }
+        x_newarr[i] = x_new;
+    }
+    for (int i=0; i < num; i++) {
+        x[i] = x_newarr[i];
+    }
+    return done;
+}
 
 /************************************************************/
 
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 
- int i;
- int nit = 0; /* number of iterations */
- FILE * fp;
- char output[100] ="";
+    int i;
+    int nit = 0; /* number of iterations */
+    FILE * fp;
+    char output[100] ="";
   
- if( argc != 2)
- {
-   printf("Usage: ./gsref filename\n");
-   exit(1);
- }
+    if( argc != 2) {
+        printf("Usage: ./gsref filename\n");
+        exit(1);
+    }
   
- /* Read the input file and fill the global data structure above */ 
- get_input(argv[1]);
+    /* Read the input file and fill the global data structure above */
+    get_input(argv[1]);
  
- /* Check for convergence condition */
- /* This function will exit the program if the coffeicient will never converge to 
-  * the needed absolute error. 
-  * This is not expected to happen for this programming assignment.
-  */
- check_matrix();
- 
- 
- 
- /* Writing results to file */
- sprintf(output,"%d.sol",num);
- fp = fopen(output,"w");
- if(!fp)
- {
-   printf("Cannot create the file %s\n", output);
-   exit(1);
- }
+    /* Check for convergence condition */
+    /* This function will exit the program if the coefficient will never converge to
+    * the needed absolute error.
+    * This is not expected to happen for this programming assignment.
+    */
+    check_matrix();
+
+    // Iterations loop
+    int foundSol = 0;
+    while (!foundSol) {
+        foundSol = 1;
+        foundSol = updateUnknowns();
+        nit++;
+    }
+
+
+    /* Writing results to file */
+    sprintf(output,"my%d.sol",num);
+    fp = fopen(output,"w");
+    if(!fp) {
+        printf("Cannot create the file %s\n", output);
+        exit(1);
+    }
     
- for( i = 0; i < num; i++)
-   fprintf(fp,"%f\n",x[i]);
+    for( i = 0; i < num; i++) {
+        fprintf(fp, "%f\n", x[i]);
+    }
+
+    printf("Unknowns\n");
+    for( i = 0; i < num; i++) {
+        printf("%f ", x[i]);
+    }
+    printf("\n");
+
+    printf("total number of iterations: %d\n", nit);
  
- printf("total number of iterations: %d\n", nit);
+    fclose(fp);
  
- fclose(fp);
- 
- exit(0);
+    exit(0);
 
 }
